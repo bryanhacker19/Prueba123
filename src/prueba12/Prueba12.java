b5758c464e7a684e5bd16974688a5fc4f5101f46
bryanhacker19 <bragameplayer123narufan@gmail.com>
2022-06-14 13:01:45 -0500

    Se agrego archivos

---
+++ src/prueba12/Prueba12.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba12;
+
+/**
+ *
+ * @author UserOne
+ */
+public class Prueba12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
